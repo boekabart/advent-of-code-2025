96843ec1e8d33ef2390a77388204b994e4efe744
Bart de Boer <bart.de.boer@divverence.com>
2025-12-02 08:33:44 +0100

    prep day3

---
+++ day3/D3P2.cs
@@ -0,0 +1,7 @@
+﻿namespace day3;
+
+public static class D3P2
+{
+    public static object Part2Answer(this string input) =>
+        new NotImplementedException();
+}
---
+++ day3/D3P2Tests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace day3;
+
+public class D3P2Tests
+{
+    [Fact(Skip = "ToDo")]
+    internal static void AcceptanceTest()
+    {
+        var expected = 42;
+        Input.ExampleInput
+            .Part2Answer()
+            .Should().Be(expected);
+    }
+
+    [Fact(Skip = "ToDo")]
+    internal static void RegressionTest()
+    {
+        var expected = 42;
+        Input.RawInput
+            .Part2Answer()
+            .Should().Be(expected);
+    }
+}
