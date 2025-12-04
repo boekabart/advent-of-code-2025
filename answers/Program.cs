f61a0804aefb5a5995839ea3771fd678e7bf6554
Bart de Boer <bart.de.boer@divverence.com>
2025-12-04 07:38:14 +0100

    prep day 5

--- answers/Program.cs
+++ answers/Program.cs
@@ -1,5 +1,15 @@
 ﻿Console.WriteLine($"Day  1 Part 1: {day1.D1P1.Part1Answer(day1.Input.RawInput)}");
 Console.WriteLine($"Day  1 Part 2: {day1.D1P2.Part2Answer(day1.Input.RawInput)}");
 
+Console.WriteLine($"Day  2 Part 1: {day2.D2P1.Part1Answer(day2.Input.RawInput)}");
+Console.WriteLine($"Day  2 Part 2: {day2.D2P2.Part2Answer(day2.Input.RawInput)}");
+
+Console.WriteLine($"Day  3 Part 1: {day3.D3P1.Part1Answer(day3.Input.RawInput)}");
+Console.WriteLine($"Day  3 Part 2: {day3.D3P2.Part2Answer(day3.Input.RawInput)}");
 
 Console.WriteLine($"Day  4 Part 1: {day4.D4P1.Part1Answer(day4.Input.RawInput)}");
+Console.WriteLine($"Day  4 Part 2: {day4.D4P2.Part2Answer(day4.Input.RawInput)}");
+
+Console.WriteLine($"Day  5 Part 1: {day5.D5P1.Part1Answer(day5.Input.RawInput)}");
+Console.WriteLine($"Day  5 Part 2: {day5.D5P2.Part2Answer(day5.Input.RawInput)}");
+
