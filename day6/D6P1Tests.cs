0f5bfe283beef2ace8efca1d364b3889488774cd
Bart de Boer <bart.de.boer@divverence.com>
2025-12-06 10:22:39 +0100

    prep day 6

---
+++ day6/D6P1Tests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace day6;
+
+public static class D6P1Tests
+{
+    [InlineData("",null)]
+    [Theory]
+    internal static void ParseInputLineTest(string line, Thing? expectedThing)
+    {
+        var actualThing = line.TryParseAsThing();
+        actualThing.Should().Be(expectedThing);
+    }
+
+    [Fact]
+    internal static void ParseInputTest()
+    {
+        var things = Input.ExampleInput.ParseThings().ToArray();
+        things.Should().HaveCount(0);
+    }
+
+    [Fact(Skip="ToDo")]
+    internal static void AcceptanceTest()
+    {
+        var expected = 42;
+        Input.ExampleInput
+            .Part1Answer()
+            .Should().Be(expected);
+    }
+
+    [Fact(Skip = "ToDo")]
+    internal static void RegressionTest()
+    {
+        var expected = 42;
+        Input.RawInput
+            .Part1Answer()
+            .Should().Be(expected);
+    }
+}
---
+++ day6/D6P2Tests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace day6;
+
+public class D6P2Tests
+{
+    [Fact(Skip = "ToDo")]
+    internal static void AcceptanceTest()
+    {
+        var expected = 42;
+        Input.ExampleInput
+            .Part2Answer()
+            .Should().Be(expected);
+    }
+
+    [Fact(Skip = "ToDo")]
+    internal static void RegressionTest()
+    {
+        var expected = 42;
+        Input.RawInput
+            .Part2Answer()
+            .Should().Be(expected);
+    }
+}
